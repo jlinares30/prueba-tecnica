c5c05ab1b1da6142abe5c8e675606f490763ce42
jlinares <jorgelinaresrrs301@gmail.com>
2026-03-16 12:38:34 -0500

    :sparkles: filtrado de busqueda añadido

---
+++ src/components/Searcher.jsx
@@ -0,0 +1,11 @@
+export default function Searcher({ value, onChange }) {
+    return (
+        <input
+            type="text"
+            placeholder="Search characters..."
+            value={value}
+            onChange={onChange}
+            className="w-full p-3 mb-6 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-green-500 transition duration-300"
+        />
+    );
+}
