7c802eb1ceb38e984aa2b6129e7756d3ece25236
jlinares <jorgelinaresrrs306@hotmail.com>
2026-03-16 21:22:54 -0500

    :sparkles: paginacion añadida

--- src/hooks/useCharacters.js
+++ src/hooks/useCharacters.js
@@ -1,18 +1,25 @@
 import { useQuery } from "@tanstack/react-query";
 import api from "../lib/api";
 
-export const useCharacters = (searchList = "") => {
+export const useCharacters = (searchList = "", page = 1) => {
   const { data, isLoading, error } = useQuery({
-    queryKey: ["characters", searchList],
+    queryKey: ["characters", searchList, page],
     queryFn: async () => {
-      const res = await api.get(`/character?name=${searchList}`); 
+      const res = await api.get(`/character`,{
+        params: {
+          name: searchList,
+          page: page
+        }
+      }
+      ); 
       console.log("Respuesta de la API:", res.data);
       if (res.data && res.data.results) {
-        return res.data.results;
+        return res.data;
       }
       
       return []; 
     },
+    keepPreviousData: true,
   });
   return { data, isLoading, error };
 };
